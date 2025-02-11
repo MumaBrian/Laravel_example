5694b7bb3dc08849da47a7735765105f52154daf
Muma Brian <brianmuma5@gmail.com>
2025-02-11 08:10:37 +0100

    updated policies

--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -5,18 +5,11 @@ namespace App\Policies;
 use App\Models\Job;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
-use Illuminate\Support\Facades\Log;
 
 class JobPolicy
 {
-    public function edit(User $user, Job $job):bool
+    public function edit(User $user, Job $job): bool
     {
-        Log::info('User ID: ' . $user->id);
-        Log::info('Employer User ID: ' . $job->employer->user->id);
-        Log::info('Employer User ID: ' . optional($job->employer->user)->id);
-
-        // return $job->employer->user->is($user);
-
-        return $job->employer && $job->employer->user && $job->employer->user->is($user);
+        return $job->employer->user->is($user);
     }
 }
